cad28dd8c51ac494f06e151a504fa0761c3d6327
Jimmy Pinkard <jimmypinkard7@gmail.com>
2024-02-29 15:18:32 -0500

    Initial Commit.

---
+++ src/main/java/com/jimmy/salaryprediction/SalaryPredictionApplication.java
@@ -0,0 +1,13 @@
+package com.jimmy.salaryprediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryPredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryPredictionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jimmy/salaryprediction/SalaryPredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jimmy.salaryprediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryPredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
