ea453677fad2411fc3f39c060e941abcff15ad18
Jimmy Pinkard <jimmypinkard7@gmail.com>
2024-03-18 21:21:28 -0400

    Added Python docker container and began refractoring ML part.

---
+++ ML/src/ai.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+
+from db import load_candidates
+
+
+def load_data():
+    column_names = "Education,Experience,Location,Job_Title,Age,Gender,Salary"
+    data = load_candidates(column_names)
+    frame = pd.DataFrame(columns=[column_names.split(',')], data=data)
+    return frame
+
+
+def train():
+    content_data = load_data()
+    content_data.drop_duplicates()
+    x = content_data.drop(columns=["Salary"])
+    y = content_data["Salary"]
+
+    regressor = DecisionTreeRegressor()
+    regressor.fit(x, y)
+    return regressor
