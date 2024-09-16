b65288c2bed435329ca979babcae9f6955954659
Jimmy Pinkard <jimmypinkard7@gmail.com>
2024-09-16 11:38:20 -0400

    Removed CORS

--- src/main/java/com/jimmy/salaryprediction/config/SecurityConfig.java
+++ src/main/java/com/jimmy/salaryprediction/config/SecurityConfig.java
@@ -8,35 +8,11 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Collections;
-import java.util.List;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig implements WebMvcConfigurer {
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        // *** URL below needs to match the Vue client URL and port ***
-        config.setAllowedOrigins(List.of(
-                "https://0.0.0.0:3000",
-                "https://127.0.0.1:3000",
-                "https://localhost:3000",
-                "https://client:3000"
-        ));
-        config.setAllowedMethods(Collections.singletonList("*"));
-        config.setAllowedHeaders(Collections.singletonList("*"));
-        source.registerCorsConfiguration("/**", config);
-        return source;
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception
@@ -44,7 +20,6 @@ public class SecurityConfig implements WebMvcConfigurer {
         return httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .requiresChannel(channel -> channel.anyRequest().requiresSecure())
                 .authorizeHttpRequests(request -> request.anyRequest().permitAll())
-                .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()))
                 .getOrBuild();
     }
 
--- src/main/java/com/jimmy/salaryprediction/controller/CandidateController.java
+++ src/main/java/com/jimmy/salaryprediction/controller/CandidateController.java
@@ -5,14 +5,14 @@ import com.jimmy.salaryprediction.controller.response.CandidateResponse;
 import com.jimmy.salaryprediction.service.CandidateService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/candidates")
-@CrossOrigin(origins = {"https://localhost:3000/", "https://client:3000/", "https://127.0.0.1:3000"})
 public class CandidateController {
     private CandidateService candidateService;
 
